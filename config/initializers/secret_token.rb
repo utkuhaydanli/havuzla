2536360526f06a4ec7e30c15f102c0704a170ba0
Utku HAYDANLI <utkuhaydanli@me.com>
2013-10-23 23:47:52 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Havuzla::Application.config.secret_key_base = 'baa1470b78b9c3544c3a79533c2196a1a15081c7b2ee018369d5d96263e0a480e0583d52c21fc270727126a9a2eb3c9fcde24a22706724c942882d2613600795'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Havuzla::Application.config.session_store :cookie_store, key: '_havuzla_session'
